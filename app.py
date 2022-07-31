e43a21beb3efa57cc6d579aee36a3ba1b8de4a0d
Pawel Romanik <pawel.romanik@gmail.com>
2022-07-31 22:02:25 +0200

    Added gitignore file

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+    
